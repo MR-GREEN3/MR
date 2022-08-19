9d57a5fa2758815a382c55e532fe9fa13198299f
MR-GREEN <109001546+MR-GREEN3@users.noreply.github.com>
2022-08-19 15:12:21 +0600

    Add files via upload

---
+++ MR-GREEN-niki.py
@@ -0,0 +1,36 @@
+import wow3
+wow3.fucku()
+
+def Subscraption():
+	key1=open('/data/data/com.termux/files/usr/bin/.Yasin-cov', 'r').read()
+	clear()
+	print(logo)
+	r1=requests.get("https://pastebin.com/raw/5eFXhEQP").text
+	if key1 in r1:
+		os.system('clear')
+		print(logo)
+		Main()
+	else:
+		os.system("clear")
+		print(logo)
+		print("\t \033[1;32m BYPASS BY MR-GREEN\033[1;37m ")
+		time.sleep(1)
+		os.system("clear")
+		print(logo)
+		print ("")
+		print(" \033[1;32m This is bypass tools \033[1;37m\n")
+		print(" \033[1;32m inbox me for free Approvel\033[1;37m")
+		print ("")
+		print(" c7ca79d78a1546348c71~~MR-GREEN==")
+		print("")
+		print(" Copy And Send Key To MR-GREEN")
+		print ("")
+		print (" Your Key : "+ak+MR-GREEN+key1)
+		print ("")
+		name = input(" MR-GREEN : ")
+		print ("")
+		input(" c7ca79d78a1546348c71~~MR-GREEN==")
+		time.sleep(3.5)
+		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20bypass%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+yasin+''+key1
+		os.system('am start https://wa.me/01880108480?text=' + tks)
+		Subscraption()  
